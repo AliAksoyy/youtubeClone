39e9253fc04052f9ee1e3fc5d39cd69327e7aa91
AliAksoyy <alifeyza4017@gmail.com>
2023-12-06 00:27:34 +0300

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,14 +4,16 @@ import { RxHamburgerMenu } from "react-icons/rx";
 
 const Header = () => {
   return (
-    <div className="w-full px-8 border-2 border-red-600 h-16 flex items-center justify-between">
+    <div className="w-full px-8 py-4 border-2 border-red-600 h-20  flex items-center justify-between">
       <div className="flex gap-x-12">
-        <RxHamburgerMenu
-          cursor={"pointer"}
-          fontSize={35}
-          color="#424040"
-          fontWeight={200}
-        />
+        <div className=" hover:bg-gray-200 hover:rounded-full p-3 transition-all">
+          <RxHamburgerMenu
+            cursor={"pointer"}
+            fontSize={35}
+            color="#424040"
+            fontWeight={200}
+          />
+        </div>
         <YoutubeIcon />
       </div>
       <div>b</div>
