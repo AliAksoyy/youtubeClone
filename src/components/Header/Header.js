762ac2636df7f8a943e0b5d22b0267a8fc312c2e
AliAksoyy <alifeyza4017@gmail.com>
2023-12-10 22:56:13 +0300

    added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import YoutubeIcon from "../Icons/YoutubeIcon";
 import { RxHamburgerMenu } from "react-icons/rx";
-
+import "./header.css";
 import RecordIcon from "../Icons/RecordIcon";
 import { IoIosSearch } from "react-icons/io";
 
 const Header = () => {
+  const [showSearch, setShowSearch] = useState(false);
+  const [showCloseIcon, setShowCloseIcon] = useState(false);
+  const inputRef = useRef(null);
+
+  const handleFocus = (e) => {
+    setShowSearch(() => true);
+  };
+
+  const handleChange = (e) => {
+    setShowCloseIcon(Boolean(e.target.value));
+  };
+
   return (
     <div className="w-full px-8 py-4 border-2 border-red-600 h-20  flex items-center justify-between">
       <div className="flex gap-x-12 basis-2/12">
-        <div className=" hover:bg-gray-200 hover:rounded-full p-3 transition-all">
+        <div className=" hover:bg-gray-200 hover:rounded-full p-3 duration-200">
           <RxHamburgerMenu
             cursor={"pointer"}
             fontSize={35}
@@ -20,23 +32,38 @@ const Header = () => {
         <YoutubeIcon />
       </div>
       <div className="basis-7/12 flex justify-around items-center py-3">
-        <div className=" flex basis-5/6 h-14 justify-between">
+        <div className="relative justify-end flex basis-5/6 h-14 ">
           <input
-            className="w-full focus:outline-none pl-7 border-2 border-stone-300 rounded-l-full text-slate-800 text-lg placeholder:text-slate-500 placeholder:text-xl focus:border-solid focus:border-cyan-800 focus:shadow-slate-400 focus:border focus:shadow-inputShadow "
+            className=" w-5/6 focus:outline-none pl-7 focus:pl-20 border-2 border-stone-300 rounded-l-full text-slate-800 text-lg placeholder:text-slate-500 placeholder:text-xl focus:border-solid focus:border-cyan-800 focus:shadow-slate-400 focus:border focus:shadow-inputShadow focus:w-full "
             type="text"
             placeholder="Ara"
-            onFocus={(e) =>
-              e.target.parentElement.classList.add("shadow-inputShadow")
-            }
-            onBlur={(e) =>
-              e.target.parentElement.classList.remove("shadow-inputShadow")
-            }
+            onFocus={handleFocus}
+            onBlur={() => setShowSearch(false)}
+            onChange={handleChange}
+            ref={inputRef}
           />
-          <div className="basis-24 flex items-center border-y-2 border-r-2 border-stone-300 justify-center w-6 bg-search rounded-r-full hover:cursor-pointer hover:bg-hoverSearch transition-all">
+          {showSearch && (
+            <div className="absolute top-1/2 left-6 -translate-y-1/2 ">
+              <IoIosSearch color="#2c2c2c" fontSize={35} fontWeight={100} />
+            </div>
+          )}
+          {showCloseIcon && (
+            <div
+              onClick={() => {
+                setShowCloseIcon(false);
+                inputRef.current.value = "";
+              }}
+              className="cursor-pointer border h-5/6 flex items-center justify-center absolute right-28 top-1 w-12 hover:bg-hoverSearch duration-200 rounded-full"
+            >
+              <div className="border-t border w-full border-zinc-900 translate-x-full -rotate-45"></div>
+              <div className=" border-t border w-full border-zinc-900 rotate-45"></div>
+            </div>
+          )}
+          <div className="basis-24 flex items-center border-y-2 border-r-2 border-stone-300 justify-center w-6 bg-search rounded-r-full hover:cursor-pointer hover:bg-hoverSearch duration-200">
             <IoIosSearch fontSize={35} />
           </div>
         </div>
-        <div className="border p-3 bg-search hover:cursor-pointer rounded-full hover:bg-hoverSearch transition-all">
+        <div className="border p-3 bg-search hover:cursor-pointer rounded-full hover:bg-hoverSearch duration-200">
           <RecordIcon />
         </div>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
         hoverSearch: "#e4e4e4",
       },
       boxShadow: {
-        inputShadow: "0 10px 15px -3px rgba(0, 0, 0, 0.1)",
+        inputShadow: "inset -4px 4px 4px -4px rgba(0, 0, 0, 0.2);",
       },
     },
   },
